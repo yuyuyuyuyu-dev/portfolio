00058fc1f3d0b05f9e0fc3a1056feee9c2a1f458
yu <email@yuyuyuyuyu.dev>
2025-08-12 12:40:55 +0900

    feat(data): update genkaikor's URL and tech stacks

--- composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/portfolio/data/repositories/AppsRepository.kt
+++ composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/portfolio/data/repositories/AppsRepository.kt
@@ -2,7 +2,27 @@ package dev.yuyuyuyuyu.portfolio.data.repositories
 
 import dev.yuyuyuyuyu.portfolio.data.models.App
 import dev.yuyuyuyuyu.portfolio.data.models.TechStack
-import portfolio.composeapp.generated.resources.*
+import portfolio.composeapp.generated.resources.CodeScanner_screenshot
+import portfolio.composeapp.generated.resources.LocationRemover_screenshot
+import portfolio.composeapp.generated.resources.PasswordGenerator_screenshot
+import portfolio.composeapp.generated.resources.Res
+import portfolio.composeapp.generated.resources.barometer_en_screenshot
+import portfolio.composeapp.generated.resources.barometer_icon
+import portfolio.composeapp.generated.resources.barometer_ja_screenshot
+import portfolio.composeapp.generated.resources.copy_to_clipboard_screenshot
+import portfolio.composeapp.generated.resources.delicioushareapp_icon
+import portfolio.composeapp.generated.resources.genkaikor_screenshot
+import portfolio.composeapp.generated.resources.get_random_int_screenshot
+import portfolio.composeapp.generated.resources.how_old_am_i_screenshot
+import portfolio.composeapp.generated.resources.howoldami_icon
+import portfolio.composeapp.generated.resources.locationremover_icon
+import portfolio.composeapp.generated.resources.not_pulling_probability_calculator_screenshot
+import portfolio.composeapp.generated.resources.puremusicplayer_icon
+import portfolio.composeapp.generated.resources.repeater_screenshot
+import portfolio.composeapp.generated.resources.to_leet_screenshot
+import portfolio.composeapp.generated.resources.what_is_the_date_today_screenshot
+import portfolio.composeapp.generated.resources.whatisthedatetoday_icon
+import portfolio.composeapp.generated.resources.ym_previewer_screenshot
 
 class AppsRepository {
     private val apps = listOf(
@@ -23,7 +43,13 @@ class AppsRepository {
             name = "今日の日付",
             description = "現在の日付を表示するアプリ",
             icon = Res.drawable.whatisthedatetoday_icon,
-            techStack = setOf(TechStack.Web, TechStack.Html, TechStack.Css, TechStack.Go, TechStack.Pwa),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.Html,
+                TechStack.Css,
+                TechStack.Go,
+                TechStack.Pwa
+            ),
             url = "https://yuyuyuyuyu-dev.github.io/what-is-the-date-today/",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/what-is-the-date-today",
             screenshots = listOf(Res.drawable.what_is_the_date_today_screenshot),
@@ -32,7 +58,12 @@ class AppsRepository {
             name = "年齢の計算",
             description = "生年月日から年齢を計算するアプリ",
             icon = Res.drawable.howoldami_icon,
-            techStack = setOf(TechStack.Web, TechStack.Kotlin, TechStack.ComposeMultiplatform, TechStack.Pwa),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.Kotlin,
+                TechStack.ComposeMultiplatform,
+                TechStack.Pwa,
+            ),
             url = "https://how-old-am-i.yuyuyuyuyu.dev",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/how-old-am-i",
             screenshots = listOf(Res.drawable.how_old_am_i_screenshot),
@@ -41,7 +72,13 @@ class AppsRepository {
             name = "Location Remover",
             description = "画像からメタデータを削除するアプリ",
             icon = Res.drawable.locationremover_icon,
-            techStack = setOf(TechStack.Web, TechStack.Html, TechStack.Css, TechStack.JavaScript, TechStack.Pwa),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.Html,
+                TechStack.Css,
+                TechStack.JavaScript,
+                TechStack.Pwa,
+            ),
             url = "https://yuyuyuyuyu-dev.github.io/location-remover/",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/location-remover",
             screenshots = listOf(Res.drawable.LocationRemover_screenshot),
@@ -49,7 +86,12 @@ class AppsRepository {
         App(
             name = "クリップボードへコピー",
             description = "入力された文字をクリップボードへコピーするアプリ",
-            techStack = setOf(TechStack.Web, TechStack.TypeScript, TechStack.React, TechStack.MaterialUi),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.TypeScript,
+                TechStack.React,
+                TechStack.MaterialUi
+            ),
             url = "https://yuyuyuyuyu-dev.github.io/copy-to-clipboard/",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/copy-to-clipboard",
             screenshots = listOf(Res.drawable.copy_to_clipboard_screenshot),
@@ -57,7 +99,12 @@ class AppsRepository {
         App(
             name = "乱数の生成",
             description = "指定した範囲から乱数を生成するアプリ",
-            techStack = setOf(TechStack.Web, TechStack.TypeScript, TechStack.React, TechStack.MaterialUi),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.TypeScript,
+                TechStack.React,
+                TechStack.MaterialUi,
+            ),
             url = "https://yuyuyuyuyu-dev.github.io/get-random-int/",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/get-random-int",
             screenshots = listOf(Res.drawable.get_random_int_screenshot),
@@ -65,15 +112,25 @@ class AppsRepository {
         App(
             name = "genkaikor",
             description = "入力された文字すべてに濁点をつけるアプリ",
-            techStack = setOf(TechStack.Web, TechStack.TypeScript, TechStack.NextJs, TechStack.MaterialUi),
-            url = "https://yuyuyuyuyu-dev.github.io/genkaikor/",
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.Kotlin,
+                TechStack.ComposeMultiplatform,
+                TechStack.Pwa,
+            ),
+            url = "https://genkaikor.yuyuyuyuyu.dev",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/genkaikor",
             screenshots = listOf(Res.drawable.genkaikor_screenshot),
         ),
         App(
             name = "repeater",
             description = "入力された文字をひたすら繰り返すアプリ",
-            techStack = setOf(TechStack.Web, TechStack.TypeScript, TechStack.NextJs, TechStack.MaterialUi),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.TypeScript,
+                TechStack.NextJs,
+                TechStack.MaterialUi,
+            ),
             url = "https://yuyuyuyuyu-dev.github.io/repeater/",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/repeater",
             screenshots = listOf(Res.drawable.repeater_screenshot),
@@ -81,7 +138,12 @@ class AppsRepository {
         App(
             name = "YM previewer for Twitter",
             description = "XでYouTube Musicのリンクをカード表示するためのワークアラウンド",
-            techStack = setOf(TechStack.Web, TechStack.NextJs, TechStack.TypeScript, TechStack.MaterialUi),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.NextJs,
+                TechStack.TypeScript,
+                TechStack.MaterialUi,
+            ),
             url = "https://ym-previewer.vercel.app",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/ym-previewer",
             screenshots = listOf(Res.drawable.ym_previewer_screenshot),
@@ -106,7 +168,12 @@ class AppsRepository {
         App(
             name = "パスワードジェネレーター",
             description = "ひらがなカタカナ対応の簡易パスワードジェネレーター",
-            techStack = setOf(TechStack.Web, TechStack.NextJs, TechStack.TypeScript, TechStack.MaterialUi),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.NextJs,
+                TechStack.TypeScript,
+                TechStack.MaterialUi,
+            ),
             url = "https://password-generator-yuyuyuyuyu-dev.vercel.app",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/password-generator",
             screenshots = listOf(Res.drawable.PasswordGenerator_screenshot),
@@ -114,7 +181,12 @@ class AppsRepository {
         App(
             name = "引けない確率の計算",
             description = "1度も引けない確率を計算するアプリ",
-            techStack = setOf(TechStack.Web, TechStack.Kotlin, TechStack.ComposeMultiplatform, TechStack.Pwa),
+            techStack = setOf(
+                TechStack.Web,
+                TechStack.Kotlin,
+                TechStack.ComposeMultiplatform,
+                TechStack.Pwa,
+            ),
             url = "https://not-pulling-probability-calculator.yuyuyuyuyu.dev",
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/not-pulling-probability-calculator",
             screenshots = listOf(Res.drawable.not_pulling_probability_calculator_screenshot),
@@ -139,7 +211,10 @@ class AppsRepository {
             icon = Res.drawable.barometer_icon,
             techStack = setOf(TechStack.Android, TechStack.Kotlin, TechStack.JetpackCompose),
             repositoryUrl = "https://github.com/yuyuyuyuyu-dev/barometer",
-            screenshots = listOf(Res.drawable.barometer_en_screenshot, Res.drawable.barometer_ja_screenshot),
+            screenshots = listOf(
+                Res.drawable.barometer_en_screenshot,
+                Res.drawable.barometer_ja_screenshot,
+            ),
         ),
         App(
             name = "コードスキャナー",
