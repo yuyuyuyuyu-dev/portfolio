a5014862a9a2e75c9f57d4cea7a884aa40c52689
yu <yu@yuyuyuyuyu.dev>
2026-03-20 09:35:13 +0900

    feat: cleanup template codes

--- composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/portfolio/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.yuyuyuyuyu.portfolio
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/jsMain/kotlin/dev/yuyuyuyuyu/portfolio/Platform.js.kt
+++
@@ -1,7 +0,0 @@
-package dev.yuyuyuyuyu.portfolio
-
-class JsPlatform: Platform {
-    override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
