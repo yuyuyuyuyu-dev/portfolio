8b58cb265ee4204e58217be2a6ed14ff449311e1
yu <yu@yuyuyuyuyu.dev>
2026-03-20 11:25:58 +0900

    feat(ui): scaffold screens

---
+++ composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/portfolio/apps/AppsScreen.kt
@@ -0,0 +1,9 @@
+package dev.yuyuyuyuyu.portfolio.ui.portfolio.apps
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun AppsScreen() {
+}
---
+++ composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/portfolio/ui/portfolio/cliTools/CliToolsScreen.kt
@@ -0,0 +1,9 @@
+package dev.yuyuyuyuyu.portfolio.ui.portfolio.cliTools
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CliToolsScreen() {
+}
