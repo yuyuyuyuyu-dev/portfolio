a8ac0d84ad32ded9a7e9acf61e3be26e499b169b
yu <yu@yuyuyuyuyu.dev>
2026-03-22 10:38:33 +0900

    build: add ViewModel libraries

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,6 +43,8 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.jetbrains.navigation3.ui)
+            implementation(libs.jetbrains.material3.adaptiveNavigation3)
+            implementation(libs.jetbrains.lifecycle.viewmodelNavigation3)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.aboutlibraries.compose)
         }
